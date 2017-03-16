79cf12a86da32ffc53a8e34a9c61aae1a0a95f34
PakoRC1 <franciscodaniel.ramirez@hotmail.com>
2017-03-15 23:20:23 -0600

    Update cachematrix.R
    Programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The idea was to modify a little bit the functions that were given as an example
+## mainly by changing from vector to matrix.
 
-## Write a short comment describing this function
+## What this function does is generating a matrix "object", i.e. 
+## a matrix that has functions to get/set its value, and to get/set its inverse.
+## We can call this function with its argument equal to some matrix, but if we leave it
+## empty, the default is an empty matrix and regardless of the choice, when we change the 
+## value of the matrix, the inverse resets to NULL.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set <- function(y) {
+    x <<- y
+    inv <<-NULL
+  }
+  get <- function() x
+  setInv <- function(I)   inv <<-I
+  getInv <- function() inv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+  
 }
 
 
-## Write a short comment describing this function
+## This function receives a matrix "object" and if it already has an inverse, it just
+## prints it, if it doesn't have one, it gets it and updates the value of the inverse of
+## the matrix "object" to the inverse.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data,...)
+  x$setInv(i)
+  i
 }
